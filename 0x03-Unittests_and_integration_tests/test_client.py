2f3aa8d6ecd4a608512fa6756ae3d178728e953b
billwk254 <billkihungu51@gmail.com>
2024-02-05 16:15:59 +0300

    Implement TestGithubOrgClient.test_has_license to unit-test GithubOrgClient.has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,40 +1,27 @@
 #!/usr/bin/env python3
 """Unit tests for client.GithubOrgClient class."""
 import unittest
-from unittest.mock import patch, Mock
+from parameterized import parameterized
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test case for GithubOrgClient class."""
 
-    @patch('client.get_json')
-    @patch.object(GithubOrgClient, '_public_repos_url', new_callable=Mock)
-    def test_public_repos(self, mock_public_repos_url, mock_get_json):
-        """Test public_repos method of GithubOrgClient."""
-        # Mocked payload for get_json
-        mocked_payload = [
-            {"name": "repo1"},
-            {"name": "repo2"},
-            {"name": "repo3"}
-        ]
-
-        # Set up the mocks
-        mock_public_repos_url.return_value = "https://api.github.com/orgs/testorg/repos"
-        mock_get_json.return_value = mocked_payload
-
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """Test has_license method of GithubOrgClient."""
         # Instantiate the GithubOrgClient
         test_client = GithubOrgClient("testorg")
 
-        # Call the public_repos method
-        result = test_client.public_repos()
+        # Call the has_license method
+        result = test_client.has_license(repo, license_key)
 
         # Assert that the result is as expected
-        self.assertEqual(result, ["repo1", "repo2", "repo3"])
-
-        # Assert that _public_repos_url property and get_json were called once
-        mock_public_repos_url.assert_called_once()
-        mock_get_json.assert_called_once_with("https://api.github.com/orgs/testorg/repos")
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
